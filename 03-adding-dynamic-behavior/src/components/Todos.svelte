3720f90994d72d353ac3bef421150c47dc3dd5c1
Sophie <sophie@sssoja.com>
2021-11-04 13:12:35 +0100

    create add todo functionality

--- 03-adding-dynamic-behavior/src/components/Todos.svelte
+++ 03-adding-dynamic-behavior/src/components/Todos.svelte
@@ -1,22 +1,42 @@
 <script>
   export let todos = [];
-
   $: totalTodos = todos.length;
   $: completedTodos = todos.filter((todo) => todo.completed).length;
-
   function removeTodo(todo) {
-    todos.filter((todo) => todo.id !== todo.id);
+    todos = todos.filter((t) => t.id !== todo.id);
   }
+
+  let newTodoId;
+  $: {
+    if (totalTodos === 0) {
+      newTodoId = 1;
+    } else {
+      newTodoId = Math.max(...todos.map((t) => t.id)) + 1;
+    }
+  }
+
+  let newTodoName = "";
+
+  function addTodo() {
+    todos = [...todos, { id: newTodoId, name: newTodoName, completed: false }];
+    newTodoName = "";
+  }
+  $: console.log(todos);
 </script>
 
 <!-- Todos.svelte -->
 <div class="todoapp stack-large">
   <!-- NewTodo -->
-  <form>
+  <form on:submit|preventDefault={addTodo}>
     <h2 class="label-wrapper">
       <label for="todo-0" class="label__lg"> What needs to be done? </label>
     </h2>
-    <input type="text" id="todo-0" autocomplete="off" class="input input__lg" />
+    <input
+      bind:value={newTodoName}
+      type="text"
+      id="todo-0"
+      autocomplete="off"
+      class="input input__lg" />
     <button type="submit" disabled="" class="btn btn__primary btn__lg">
       Add
     </button>
@@ -41,6 +61,14 @@
     </button>
   </div>
 
+  <!-- TodosStatus -->
+  <h2 id="list-heading">
+    {completedTodos}
+    out of
+    {totalTodos}
+    items completed
+  </h2>
+
   <!-- Todos -->
   <ul role="list" class="todo-list stack-large" aria-labelledby="list-heading">
     {#each todos as todo (todo.id)}
@@ -51,7 +79,7 @@
               type="checkbox"
               id="todo-{todo.id}"
               checked={todo.completed}
-              onclick={() => (todo.completed = !todo.completed)} />
+              on:click={() => (todo.completed = !todo.completed)} />
             <label for="todo-{todo.id}" class="todo-label"> {todo.name} </label>
           </div>
           <div class="btn-group">
